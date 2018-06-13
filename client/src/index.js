3b6739391d6d6ebfd51e36280b1210ee5bf4a6b2
Joey Feng <jfeng702@gmail.com>
2018-06-13 14:59:51 -0700

    survey templates

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,10 @@ import ReduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
+// development only axios helpers!
+import axios from 'axios';
+window.axios = axios;
+
 
 const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
 
