0b7cfcb7045ffecef6863f06ac8835b5ccf9607f
Joey Feng <jfeng702@gmail.com>
2018-06-22 18:00:34 -0700

    last update

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,13 +10,20 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req, res) => {
+  app.get('/api/surveys', requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id })
+      .select({ recipients: false });
+
+    res.send(surveys);
+  });
+
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks for voting!');
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
     const p = new Path('/api/surveys/:surveyId/:choice');
-    const events = _.chain(req.body)
+    _.chain(req.body)
       .map(({ url, email }) => {
         const match = p.test(new URL(url).pathname);
         if (match) {
@@ -25,9 +32,20 @@ module.exports = app => {
       })
       .compact()
       .uniqBy('email', 'surveyId')
+      .each(({ email, choice, surveyId }) => {
+        Survey.updateOne({
+          _id: surveyId,
+          recipients: {
+            $elemMatch: { email: email, responded: false }
+          }
+        }, {
+          $inc: { [choice]: 1},
+          $set: { 'recipients.$.responded': true },
+          lastResponded: new Date()
+        }).exec();
+      })
       .value();
 
-    console.log(events);
     res.send({});
   });
 
